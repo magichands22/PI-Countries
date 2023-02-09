639c8014c69fda781bc5d17804564bc2615a6ba3
agustin cordoba <cordobajagustin@gmail.com>
2023-02-08 23:10:18 -0300

    cambios finales

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import store from './redux/store/index'
-import "@madzadev/audio-player/dist/index.css";
+
 
 ReactDOM.render(
   <Provider store={store}>
