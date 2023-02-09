ed68fdd4bec1fcaea7c695527d937a851dee283a
A.Cordoba <cordobajagustin@gmail.com>
2023-02-09 11:58:38 -0300

    reestructuring

--- api/index.js
+++ api/index.js
@@ -19,11 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const port = process.env.PORT || 3001;
+require('dotenv').config();
+const {PORT} = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
+  server.listen(PORT, () => {
+    console.log('%s listening at 3001', process.env.PORT); // eslint-disable-line no-console
+  }); 
 });
